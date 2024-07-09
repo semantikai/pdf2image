3024fa1db61f6bdcc88c6c9c7a550af42f01b97e
ASEKAK <abdelfattah.sekak@consulting-for.accor.com>
2024-07-09 20:31:14 +0200

    wip

--- apps/web/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-interface AuthLayoutProps {
-  children: React.ReactNode
-}
-
-export default function AuthLayout({ children }: AuthLayoutProps): JSX.Element {
-  return <div className="min-h-screen">{children}</div>
-}
