e33b235f6ce860fb7409943137e9ac95adbba240
ASEKAK <abdelfattah.sekak@consulting-for.accor.com>
2024-07-18 13:17:22 +0200

    chore: Clean up unused files and configurations

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mantine/core";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/hello" passHref>
+        <Button component="a">Next link button</Button>
+      </Link>
+    </main>
+  );
+}
